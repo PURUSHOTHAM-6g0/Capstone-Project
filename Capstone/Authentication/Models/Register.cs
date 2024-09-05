46c28b2eebb264247073fde4aacdccc5498b96f1
Purushotham.Perumala@ust.com <Purushotham.Perumala@ust.com>
2024-09-05 11:37:37 +0530

    Models Created

---
+++ Capstone/Authentication/Models/Register.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Authentication.Models
+{
+    public class Register:IdentityUser
+    {
+        [Required]
+        public string EmployeeId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Country { get; set; }
+        public string TimeZone { get; set; }
+    }
+}
---
+++ Capstone/Authentication/Models/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace Authentication.Models
+{
+    public class Roles
+    {
+        public const string Admin = "Manager";
+        public const string User = "Employee";
+    }
+}
