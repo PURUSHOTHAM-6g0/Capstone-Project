35917170f3c4ade8516c09d8d30fd57fc122e12f
Purushotham.Perumala@ust.com <Purushotham.Perumala@ust.com>
2024-09-05 17:30:22 +0530

    Completed Authentication, Registartion and Login methods

--- Capstone/Authentication/Program.cs
+++ Capstone/Authentication/Program.cs
@@ -1,5 +1,10 @@
 using Authentication.Data;
+using Authentication.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Authentication
 {
@@ -8,19 +13,44 @@ namespace Authentication
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("Enable Cors", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+            builder.Services.AddIdentity<Register, IdentityRole>().AddEntityFrameworkStores<AuthenticationDbContext>().AddDefaultTokenProviders();
 
             // Add services to the container.
             builder.Services.AddDbContext<AuthenticationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AuthenticationDBString")));
 
             builder.Services.AddControllers();
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["JWTAuth:ValidAudienceURL"],
+                    ValidIssuer = builder.Configuration["JWTAuth:ValidIssuerURL"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTAuth:SecretKey"]))
+                };
+            });
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-
+            app.UseCors("Enable Cors");
+            app.UseAuthentication();
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
---
+++ Capstone/Authentication/Repository/AuthenticationRepository.cs
@@ -0,0 +1,157 @@
+﻿using Authentication.Contracts;
+using Authentication.Dto;
+using Authentication.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Authentication.Repository
+{
+    public class AuthenticationRepository : IAuthentication
+    {
+        private readonly UserManager<Register> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        public AuthenticationRepository(UserManager<Register> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+        public async Task<Response> AdminRegister(Register model)
+        {
+            var userExists = await _userManager.FindByIdAsync(model.EmployeeId);
+            var emailExists=await _userManager.FindByEmailAsync(model.Email);
+            string timeZone = await GetTimeZoneFromCountry(model.Country);
+            if (userExists != null || emailExists!=null)
+                return new Response { Status = "Error", Message = "User already exists!" };
+            Register user = new()
+            {
+                EmployeeId = model.EmployeeId,
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.UserName,
+                Name = model.Name,
+                Country = model.Country,
+                TimeZone = timeZone
+            };
+            var result = await _userManager.CreateAsync(user, model.PasswordHash);
+            if (!result.Succeeded)
+                return new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." };
+            if (!await _roleManager.RoleExistsAsync(Roles.Admin))
+                await _roleManager.CreateAsync(new IdentityRole(Roles.Admin));
+            if (!await _roleManager.RoleExistsAsync(Roles.User))
+                await _roleManager.CreateAsync(new IdentityRole(Roles.User));
+            if (await _roleManager.RoleExistsAsync(Roles.Admin))
+            {
+                await _userManager.AddToRoleAsync(user, Roles.Admin);
+            }
+            if (await _roleManager.RoleExistsAsync(Roles.User))
+            {
+                await _userManager.AddToRoleAsync(user, Roles.User);
+            }
+            return new Response { Status = "Success", Message = "User created successfully!" };
+        }
+
+        public async Task<object> CheckLoginCred(Login model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+                var token = GetToken(authClaims);
+                return new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                };
+            }
+            return null;
+        }
+
+        public async Task<GetEmployeeDto> GetEmployeeById(string id)
+        {
+            var getEmployee = await _userManager.FindByIdAsync(id);
+            GetEmployeeDto employee = new GetEmployeeDto()
+            {
+                Email = getEmployee.Email,
+                EmployeeId = getEmployee.EmployeeId,
+                Name = getEmployee.Name,
+                TimeZone = getEmployee.TimeZone
+            };
+            return employee;
+        }
+
+        public async Task<Response> UserRegister(Register model)
+        {
+            var userExists = await _userManager.FindByIdAsync(model.EmployeeId);
+            var emailExists = await _userManager.FindByEmailAsync(model.Email);
+            string timeZone = await GetTimeZoneFromCountry(model.Country);
+            if (userExists != null || emailExists != null)
+                return new Response { Status = "Error", Message = "User already exists!" };
+            Register user = new()
+            {
+                EmployeeId = model.EmployeeId,
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.UserName,
+                Name = model.Name,
+                Country = model.Country,
+                TimeZone = timeZone
+            };
+            var result = await _userManager.CreateAsync(user, model.PasswordHash);
+            if (!result.Succeeded)
+            {
+                return new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." };
+            }
+            return new Response { Status = "Success", Message = "User created successfully!" };
+        }
+        private JwtSecurityToken GetToken(List<Claim> authClaims)
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWTAuth:SecretKey"]));
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWTAuth:ValidIssuerURL"],
+                audience: _configuration["JWTAuth:ValidAudienceURL"],
+                expires: DateTime.Now.AddHours(3),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256));
+            return token;
+        }
+
+        private async Task<string> GetTimeZoneFromCountry(string isoCountryCode)
+        {
+            string apiKey = "7Q2Q2081QMOO";
+            using (var httpClient = new HttpClient())
+            {
+                string apiUrl = $"https://api.timezonedb.com/v2.1/list-time-zone?key={apiKey}&format=json&country={isoCountryCode}";
+
+                HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseData = await response.Content.ReadAsStringAsync();
+                    var jsonResponse = JsonConvert.DeserializeObject<dynamic>(responseData);
+                    var timeZones = jsonResponse?.zones;
+                    return timeZones?[0]?.zoneName?.ToString();
+                }
+            }
+            return null;
+        }
+
+    }
+}
