a1c493cecc135437318e75191069daf6e961fdac
Purushotham.Perumala@ust.com <Purushotham.Perumala@ust.com>
2024-09-05 12:02:18 +0530

    Added contracts and created dto

---
+++ Capstone/Authentication/Contracts/IAuthentication.cs
@@ -0,0 +1,16 @@
+﻿using Authentication.Dto;
+using Authentication.Models;
+
+namespace Authentication.Contracts
+{
+    public interface IAuthentication
+    {
+        public Task<object> CheckLoginCred(Login model);
+
+        public Task<Response> UserRegister(Register model);
+
+        public Task<Response> AdminRegister(Register model);
+
+        public Task<GetEmployeeDto> GetEmployeeById(string id);
+    }
+}
---
+++ Capstone/Authentication/Dto/GetEmployeeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Authentication.Dto
+{
+    public class GetEmployeeDto
+    {
+        public string EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string TimeZone { get; set; }
+        public string Email {  get; set; }
+    }
+}
