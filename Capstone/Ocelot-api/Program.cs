308562b1731525617c0bbb6770526daa19bc028a
Purushotham.Perumala@ust.com <Purushotham.Perumala@ust.com>
2024-09-06 21:05:36 +0530

    Added CORS policy

--- Capstone/Ocelot-api/Program.cs
+++ Capstone/Ocelot-api/Program.cs
@@ -2,12 +2,20 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Enable Cors", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
 builder.Configuration.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
 
-
+app.UseCors("Enable Cors");
 await app.UseOcelot();
+
 
 app.Run();
